d0831e6cd8989bcc3bbb20e4402021f87c1b58c2
Cory <cory.brighton@allata.com>
2023-03-11 13:17:44 -0700

    changes to mock data

--- src/app/mock-data/mockBudgets.ts
+++ src/app/mock-data/mockBudgets.ts
@@ -1,67 +1,10 @@
 import { BudgetCategory } from "../models/budget-category";
 
 export const MockBudgetCategories: BudgetCategory[] = [
-  {
-    name: "Savings",
-    amount: 1000,
-    lastMonthBudgetedAmount: 1000,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Food",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Mortgage",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Utilities",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Transportation",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Insurance",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: true
-  },
-  {
-    name: "Medical",
-    amount: 500,
-    lastMonthBudgetedAmount: 800,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Dogs",
-    amount: 1000,
-    lastMonthBudgetedAmount: 1000,
-    spent: 200,
-    monthlyRecurring: false
-  },
-  {
-    name: "Hot Tub",
-    amount: 500,
-    lastMonthBudgetedAmount: 500,
-    spent: 500,
-    monthlyRecurring: true
-  }
+  new BudgetCategory("Savings", 1000, 1000, 1000),
+  new BudgetCategory("Food", 500, 800, 200, 1000),
+  new BudgetCategory("Mortgage", 2000, 2000, 2000),
+  new BudgetCategory("Utilities", 250, 150, 100),
+  new BudgetCategory("Dog"),
+
 ]
--- src/app/models/budget-category.ts
+++ src/app/models/budget-category.ts
@@ -1,15 +1,30 @@
-export class BudgetCategory {
+interface BudgetCategoryInterface{
   name: string;
-  amount: number;
-  spent: number;
-  lastMonthBudgetedAmount: number;
-  monthlyRecurring: boolean;
+  amount?: number;
+  spent?: number;
+  lastMonthSpent?: number;
+  lastMonthBudgetedAmount?: number;
+  monthlyRecurring?: boolean;
+}
 
-  constructor(name: string, amount: number, spent: number, lastMonthBudgetedAmount: number, monthlyRecurring: boolean) {
+export class BudgetCategory implements BudgetCategoryInterface{
+  name: string;
+  amount?: number;
+  spent?: number;
+  lastMonthSpent?: number;
+  lastMonthBudgetedAmount?: number;
+  monthlyRecurring?: boolean;
+
+  constructor(name: string, amount = 0, spent = 0, lastMonthBudgetedAmount = 0, lastMonthSpent = 0, monthlyRecurring = false) {
     this.name = name;
     this.amount = amount;
     this.spent = spent;
     this.lastMonthBudgetedAmount = lastMonthBudgetedAmount;
+    this.lastMonthSpent = lastMonthSpent;
     this.monthlyRecurring = monthlyRecurring;
+  }
+
+  showAll() {
+    return Object.keys(this)
   }
 }
